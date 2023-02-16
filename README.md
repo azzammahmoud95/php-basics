10e7955e41545fc2510febb7caa22cd43f53adc5
Azzam Mahmoud <azzammahmoud007@gmail.com>
2023-02-16 20:15:20 +0200

    Step5

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # php-basics
+## It includes the following files
+1. The basics 1
+2. The basics 2
+3. The basics 3
+4. The basics 4
+
+__Note:__ **Each file contains all committed steps needed to know about php-basics**
--- basics1.php
+++ basics1.php
@@ -1,23 +1,8 @@
 
 <?php 
-$name = "azzam";
-$position = "junior developer";
-$github_url = "https://github.com/azzam";
-// first method
-echo "My name is " . $name . " and position is " . $position . " please check my github account " . $github_url . "<br />";
-// second method
-echo "My name is {$name} and position is {$position} please check my github account {$github_url} <br />";
-// third method
-$str = "My name is ";
-$str .= $name;
-$str .= " and position is ";
-$str .= $position;
-$str .= " please check my github account ";
-$str .= $github_url;
-echo  $str; 
-
-// fourth method
-$arr = array(" <br>My name is ", $name, " and position is ", $position, " please check my github account " , $github_url);
-$string  = join(" " ,$arr );
-echo  $string;  
+ $course_name  = "PHP Basic Course";
+ $enrolled_students = 41;
+ $price = 200.12;
+ $on_discount = false;
+echo "Course title: " . $course_name . "<br>" ."Enrolled students: " . $enrolled_students . "<br>" ."Course price: ". $price  . " $ USD <br> " ."Course on discount: YES / NO". $on_discount
  ?>
