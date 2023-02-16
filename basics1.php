1d6e879a1f9ba19b142f1f5c505a881e37e94bac
Azzam Mahmoud <azzammahmoud007@gmail.com>
2023-02-16 19:21:03 +0200

    step4 with optional concatenation

--- basics1.php
+++ basics1.php
@@ -1,11 +1,23 @@
 
 <?php 
 $name = "azzam";
-$email = "azzam@gmail.com";
-$width = 12;
-$height = 10;
-$area = $width * $height;
-echo "hello my name is " . $name . " and my email is " . $email ."<br />";
-echo " The area is ". $area;
+$position = "junior developer";
+$github_url = "https://github.com/azzam";
+// first method
+echo "My name is " . $name . " and position is " . $position . " please check my github account " . $github_url . "<br />";
+// second method
+echo "My name is {$name} and position is {$position} please check my github account {$github_url} <br />";
+// third method
+$str = "My name is ";
+$str .= $name;
+$str .= " and position is ";
+$str .= $position;
+$str .= " please check my github account ";
+$str .= $github_url;
+echo  $str; 
 
+// fourth method
+$arr = array(" <br>My name is ", $name, " and position is ", $position, " please check my github account " , $github_url);
+$string  = join(" " ,$arr );
+echo  $string;  
  ?>
