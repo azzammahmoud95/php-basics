d91fc25d759f2e0eba85549f4b9546475a939614
Azzam Mahmoud <azzammahmoud007@gmail.com>
2023-02-16 18:31:57 +0200

    step2

--- basics1.php
+++ basics1.php
@@ -1,3 +1,4 @@
-"Hello world, Help me I am using PHP"
-<?php
+<h1>Hello world, Help me I am using PHP</h1>
+<?php echo "I am getting errors but I can't see the errors";
+print "\r\n Search on how to enable PHP Errors";
  ?>
