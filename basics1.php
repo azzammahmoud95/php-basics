ab4c7d2089c2c41f3a206cbc4403c72283d0f732
Azzam Mahmoud <azzammahmoud007@gmail.com>
2023-02-17 14:48:31 +0200

    Step8-b

--- basics1.php
+++ basics1.php
@@ -1,5 +1,5 @@
 
 <?php 
-$grocery = array("Eggs", "Milk" , "Cheese" , "Water Pack", "Tissues", "Watermelon");
-echo "Hello Sir, do you have {$grocery[0]}, {$grocery[1]}, and {$grocery[2]}? Also if you sell fruits can I find a {$grocery[5]}?";
+$grocery_list_keyed=array("eggs"=> array("balade", "mazare3"), "Milk" => array('Fresh', 'Taanayel'),"Water Pack" => array("Reem", "Tannourin"));
+echo "Hey Sir, Please I need 1 pack of {$grocery_list_keyed["eggs"][1]} eggs and 3 {$grocery_list_keyed["Water Pack"][0]} Water Pack";
  ?>
