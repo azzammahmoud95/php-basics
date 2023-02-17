97152462684c29f363cabf92dbaa2005f7f0263b
Azzam Mahmoud <azzammahmoud007@gmail.com>
2023-02-17 14:55:03 +0200

    Step9

--- basics1.php
+++ basics1.php
@@ -1,5 +1,7 @@
 
 <?php 
-$grocery_list_keyed=array("eggs"=> array("balade", "mazare3"), "Milk" => array('Fresh', 'Taanayel'),"Water Pack" => array("Reem", "Tannourin"));
-echo "Hey Sir, Please I need 1 pack of {$grocery_list_keyed["eggs"][1]} eggs and 3 {$grocery_list_keyed["Water Pack"][0]} Water Pack";
+$name = "azzam";
+var_dump($name);
+$another_variable = "hi";
+var_dump($another_variable);
  ?>
