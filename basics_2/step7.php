11f2964a7f05b8c1b5a0bdc5d2fd17557c29e920
Azzam Mahmoud <azzammahmoud007@gmail.com>
2023-02-18 20:13:22 +0200

    basic2 step7

---
+++ basics_2/step7.php
@@ -0,0 +1,13 @@
+<?php
+// Input string
+$inputString = "Python is a popular programming language for data science and machine learning.";
+
+// Replace "Python" with "PHP"
+$inputString = str_replace("Python", "PHP", $inputString);
+
+// Replace "PHP" with " PHP"
+$inputString = str_replace("PHP", " PHP", $inputString);
+
+// Output the modified string
+echo $inputString;
+?>
