63dc91c388d62a25180d99626ca219950d989a5d
Azzam Mahmoud <azzammahmoud007@gmail.com>
2023-02-19 12:21:48 +0200

    basic2 step11

---
+++ basics_2/step11.php
@@ -0,0 +1,12 @@
+<?php
+function isPrime($num){
+    if($num <= 1){return false;}
+
+    for($i = 2;$i <= $num/2; $i++){
+        if($num % $i == 0){return false;}
+    }
+    return true;
+
+}
+isPrime(15);
+?>
